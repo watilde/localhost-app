69e252d9858bd531ca694fc0ba2a04e3ee405744
Daijiro Wachi <daijiro.wachi@gmail.com>
2020-01-02 20:38:57 +0900

    integrate ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  Avatar,
+  Button,
+  TextField,
+  Link,
+  Box,
+  Typography,
+  Grid
+} from "@material-ui/core";
+import WifiIcon from "@material-ui/icons/Wifi";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+  const [port, setPort] = useState(3000);
+  const handlePort = e => {
+    setPort(Number(e.target.value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <WifiIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          localhost.app
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="file"
+            label="Root directory"
+            type="file"
+            name="file"
+            autoFocus
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="port"
+            label="Port"
+            type="number"
+            id="port"
+            value={port}
+            onChange={handlePort}
+            InputLabelProps={{
+              shrink: true
+            }}
+            inputProps={{
+              min: "0",
+              max: "65535",
+              step: "1"
+            }}
+          />
+        </form>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="secondary"
+              className={classes.submit}
+            >
+              Stop
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Start
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+      <Box mt={4}>
+        <Typography variant="body2" color="textSecondary" align="center">
+          {"© "}
+          <Link href="https://twitter.com/watilde">Daijiro Wachi</Link>
+        </Typography>
+      </Box>
+    </Container>
   );
 }
-
-export default App;
